4ce2e544d87de9ae804f1352e14495cd28fddfe1
Replit user <>
2023-03-08 15:25:27 +0530

    updated index.js with logic required for the assignment

--- index.js
+++ index.js
@@ -8,25 +8,81 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/', function(req, res) {
+  res.json({
+    "unix": Date.parse(new Date()),
+    "utc": new Date().toUTCString()
+  });
+})
+
+app.get('/api/:date', function(req, res) {
+  // if (req.params.date) {
+  //   const date = new Date(req.params.date);
+
+  //   if (date) {
+  //     const unixDate = Date.parse(date);
+  //     const utcDate = date.toUTCString();
+
+  //     if (dateIsValid(date)) {
+  //       res.json({
+  //         "unix": unixDate,
+  //         "utc": utcDate
+  //       });
+  //     } else {
+  //       res.json({ "error": "Invalid Date" });
+  //     }
+  //   } else {
+  //     res.json({
+  //       "unix": Date.parse(req.params.date),
+  //       "utc": req.params.date.toUTCString()
+  //     })
+  //   }
+
+  // }
+
+  // function dateIsValid(date) {
+  //   return date instanceof Date && !isNaN(date);
+  // }
+
+  const dateString = req.params.date;
+
+  if(dateString === undefined || dateString.trim() === '') {
+    res.json({
+      "unix": new Date().getTime(),
+      "utc": new Date().toUTCString()
+    })
+  } else {
+    const date = !isNaN(dateString) ? new Date(parseInt(dateString)) : new Date(dateString);
+
+    if (!isNaN(date.getTime())) { 
+      // timestamp = getTimestamp(date);
+      res.json({  "unix": date.getTime(),  "utc": date.toUTCString()})
+    } else {
+      res.json({
+        "error": "Invalid Date"
+      })    
+    }
+  }
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
